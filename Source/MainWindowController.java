d16eb4f129c064c5f06eacd90b4d8f0a77228787
alexey-belmach <livorni@mail.ru>
2015-11-11 23:22:55 +0300

    Created main interface

---
+++ Source/MainWindowController.java
@@ -0,0 +1,80 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class MainWindowController {
+    public void clickOpen(ActionEvent actionEvent) {
+    }
+
+    public void clickSave(ActionEvent actionEvent) {
+    }
+
+    public void clickPreview(ActionEvent actionEvent) {
+    }
+
+    public void clickHead(ActionEvent actionEvent) {
+    }
+
+    public void clickBody(ActionEvent actionEvent) {
+    }
+
+    public void clickHtml(ActionEvent actionEvent) {
+    }
+
+    public void clickTitle(ActionEvent actionEvent) {
+    }
+
+    public void clickLink(ActionEvent actionEvent) {
+    }
+
+    public void clickH6(ActionEvent actionEvent) {
+    }
+
+    public void clickH5(ActionEvent actionEvent) {
+    }
+
+    public void clickH4(ActionEvent actionEvent) {
+    }
+
+    public void clickH2(ActionEvent actionEvent) {
+    }
+
+    public void clickH1(ActionEvent actionEvent) {
+    }
+
+    public void clickH3(ActionEvent actionEvent) {
+    }
+
+    public void clickStrong(ActionEvent actionEvent) {
+    }
+
+    public void clickI(ActionEvent actionEvent) {
+    }
+
+    public void clickB(ActionEvent actionEvent) {
+    }
+
+    public void clickEm(ActionEvent actionEvent) {
+    }
+
+    public void clickA(ActionEvent actionEvent) {
+    }
+
+    public void clickDiv(ActionEvent actionEvent) {
+    }
+
+    public void clickSpan(ActionEvent actionEvent) {
+    }
+
+    public void clickTable(ActionEvent actionEvent) {
+    }
+
+    public void clickUl(ActionEvent actionEvent) {
+    }
+
+    public void clickOl(ActionEvent actionEvent) {
+    }
+
+    public void clickExit(ActionEvent actionEvent) {
+    }
+}
